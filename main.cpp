2151fe478db991074d1cdea7c5260058cea22384
Pavel Lysenko <pavellysenko@Mac-mini-Pavel.local>
2017-10-08 06:38:52 +0300

    resource path fixed

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 
+#include "commonPaths.h"
+
 #include "peoplegroup.h"
 #include "politicalgroup.h"
 #include "somecountry.h"
@@ -17,7 +19,11 @@ int main(int argc, char *argv[])
     qmlRegisterType<PeopleGroup>("modules.vev",1,0,"PeopleGroup");
 
     engine.addImportPath("qrc:/");
-    engine.rootContext()->setContextProperty("applicationDir",app.applicationDirPath());
+#if (QT_VERSION == QT_VERSION_CHECK(5, 9, 2))
+    engine.rootContext()->setContextProperty("resourceDir",app.applicationDirPath() + "/../Resources/constAppData");
+#else
+    engine.rootContext()->setContextProperty("resourceDir",FKUtility::dataDir().absolutePath());
+#endif
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
